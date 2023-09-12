c4727830dbf0a35a86c55fbd0c74e37306dc0edf
337yj <337yje@gmail.com>
2023-09-12 22:45:11 +0900

    style: 레이아웃, 버튼 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useSearchParams } from "react-router-dom";
 import useFetchData from "./hooks/useFetchData";
 import Chart from "./components/Chart";
+import styled from "styled-components";
 
 function App() {
   const { data } = useFetchData();
@@ -21,22 +22,55 @@ function App() {
   };
 
   return (
-    <div>
-      {distinctIds.map((id) => {
-        return (
-          <button key={id} onClick={() => onClickFilter(id)}>
-            {id}
-          </button>
-        );
-      })}
-
-      <Chart
-        data={data}
-        setSearchParams={setSearchParams}
-        selectedId={selectedId}
-      />
-    </div>
+    <Container>
+      <ButtonContainer>
+        {distinctIds.map((id) => {
+          return (
+            <Button key={id} onClick={() => onClickFilter(id)}>
+              {id}
+            </Button>
+          );
+        })}
+      </ButtonContainer>
+      <ChartContainer>
+        <Chart
+          data={data}
+          setSearchParams={setSearchParams}
+          selectedId={selectedId}
+        />
+      </ChartContainer>
+    </Container>
   );
 }
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const ButtonContainer = styled.div`
+  width: 480px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Button = styled.button`
+  padding: 14px;
+  margin-bottom: 32px;
+  border-radius: 42px;
+  background-color: rgba(150, 150, 150, 0.6);
+  cursor: pointer;
+  &:hover,
+  &:focus {
+    background-color: rgba(255, 200, 20, 1);
+  }
+`;
+
+export const ChartContainer = styled.div`
+  width: 100%;
+`;
 
 export default App;
